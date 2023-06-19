4cd51009317dbce5b010ddf45c38bd2c7c827e1e
MD-ANIKS <freelanceranik1879@gmail.com>
2023-06-20 01:00:17 +0600

    Implement increment and decrement functionality with Material UI buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Tooltip from '@mui/material/Tooltip';
+import Zoom from '@mui/material/Zoom';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  
+  const incValue = () => {
+    setCounter(counter + 1);
+  }
+
+  const decValue = () => {
+    setCounter( counter > 0 ? counter - 1 : counter );
+    if(counter === 0) {
+      alert(' Sorry ! Value is not decreases ')
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>#7. Increment & Decrement Challenge In React Js</h1>
       </header>
-    </div>
+      <main>
+          <div className="container">
+            <div className="box-area">
+              <div className="counter">
+                  <h1 className="count" style={{color: counter === 0 ? 'red' : '#f1f1f1'}}>{counter}</h1>
+              </div>
+              <div className='buttons'>
+                <Tooltip title="Add" TransitionComponent={Zoom} arrow>
+                    <Button onClick={incValue} className='incBtn' > <AddIcon/> </Button>
+                </Tooltip>
+                <Tooltip title="Delete" TransitionComponent={Zoom} arrow>
+                    <Button className='decBtn' onClick={decValue} > <DeleteIcon/> </Button>
+                </Tooltip>
+              </div>
+            </div>
+          </div>
+      </main>
+    </>
   );
 }
 
